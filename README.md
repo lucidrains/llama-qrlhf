71c43d1aac7e8738c627bfb8b29f3d5c1f0a7b21
lucidrains <lucidrains@gmail.com>
2023-12-02 08:20:21 -0800

    first let us try the formulation in the q-robotic-transformer paper

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# llama-qrlhf
+## Llama - QRLHF
 
-Implementation of the Llama architecture with RLHF + Q-learning
+Implementation of the Llama (or any language model) architecture with RLHF + Q-learning.
+
+Will start off by adapting the autoregressive discrete Q-learning formulation in the cited paper below and run a few experiments on arithmetic, using a symbolic solver as reward generator.
+
+## Citations
+
+```bibtex
+@inproceedings{qtransformer,
+    title   = {Q-Transformer: Scalable Offline Reinforcement Learning via Autoregressive Q-Functions},
+    authors = {Yevgen Chebotar and Quan Vuong and Alex Irpan and Karol Hausman and Fei Xia and Yao Lu and Aviral Kumar and Tianhe Yu and Alexander Herzog and Karl Pertsch and Keerthana Gopalakrishnan and Julian Ibarz and Ofir Nachum and Sumedh Sontakke and Grecia Salazar and Huong T Tran and Jodilyn Peralta and Clayton Tan and Deeksha Manjunath and Jaspiar Singht and Brianna Zitkovich and Tomas Jackson and Kanishka Rao and Chelsea Finn and Sergey Levine},
+    booktitle = {7th Annual Conference on Robot Learning},
+    year   = {2023}
+}
+```
--- llama_qrlhf/llama_qrlhf.py
+++ llama_qrlhf/llama_qrlhf.py
@@ -6,10 +6,100 @@ from einops import rearrange, repeat
 
 from beartype import beartype
 
+from torchtyping import TensorType
+
 # helper functions
 
 def exists(v):
     return v is not None
+
+# tensor helpers
+
+def batch_select_indices(t, indices):
+    indices = rearrange(indices, '... -> ... 1')
+    selected = t.gather(-1, indices)
+    return rearrange(selected, '... 1 -> ...')
+
+# Q functions
+
+# autoregressive formulation from https://qtransformer.github.io/
+# adapted to language modeling
+
+def autoregressive_q_learn(
+    model:          Module,
+    ema_model:      Module,
+    states:         TensorType['b', 'n', int],     # the entire sequence, containing prompt and generated
+    prompt_len:     TensorType['b', int],          # the length of the sequence that is the preceding prompt
+    next_states:    TensorType['b', int],          # selected action becomes the next state
+    rewards:        TensorType['b', 'n', float],   # the reward could be given at the very end, or interspersed (say the language model made a first correct reasoning then fails later on)
+    eos_id:         Optional[int] = None,          # calculate the done from the <eos> token id
+    discount_gamma: float = 0.998                  # reward discount factor, encourages brevity of generated answer
+
+) -> Tensor:
+    """
+    einops
+
+    b - batch
+    n - sequence len
+    """
+
+    batch, seq_len = states.shape, states.device
+
+    # anything after the first done flag will be considered terminal
+
+    if exists(eos_id):
+        done = states == eos_id
+        dones = dones.cumsum(dim = -1) > 0
+        dones = F.pad(dones, (1, -1), value = False)
+
+        not_terminal = (~dones).float()
+
+        # rewards should not be given on and after terminal step
+
+        rewards = rewards * not_terminal
+
+    # because greek unicode is nice to look at
+
+    γ = self.discount_factor_gamma
+
+    # get predicted Q for each action
+
+    q_pred_all_actions = model(states)
+    q_pred = batch_select_indices(q_pred_all_actions, actions)
+
+    # get q_next
+
+    q_next = ema_model(next_states)
+    q_next = q_next.max(dim = -1).values
+
+    # get target Q
+
+    q_target_all_actions = ema_model(states)
+    q_target = q_target_all_actions.max(dim = -1).values
+
+    # main contribution of the paper is the following logic
+    # section 4.1 - eq. 1
+
+    # first take care of the loss for all actions except for the very last one
+
+    q_pred_rest_actions, q_pred_last_action      = q_pred[..., :-1], q_pred[..., -1]
+    q_target_first_action, q_target_rest_actions = q_target[..., 0], q_target[..., 1:]
+
+    losses_all_actions_but_last = F.mse_loss(q_pred_rest_actions, q_target_rest_actions, reduction = 'none')
+
+    # next take care of the very last action, which incorporates the rewards
+
+    q_target_last_action, _ = pack([q_target_first_action[..., 1:], q_next], 'b *')
+
+    q_target_last_action = rewards + γ * q_target_last_action
+
+    losses_last_action = F.mse_loss(q_pred_last_action, q_target_last_action, reduction = 'none')
+
+    # flatten and average
+
+    losses, _ = pack([losses_all_actions_but_last, losses_last_action], '*')
+
+    return losses.mean()
 
 # main classes
 
